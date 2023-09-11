8928515412d43978a6ea98e4feed4946d6b00e1b
sumanmeesala <129158238+sumanmeesala@users.noreply.github.com>
2023-09-11 12:17:01 +0530

    Add files via upload

---
+++ 1st.groovy
@@ -0,0 +1,77 @@
+package hudson.cli;
+
+import hudson.model.*;
+import jenkins.model.Jenkins;
+import hudson.model.ListView;
+import jenkins.model.GlobalConfiguration;
+
+def environment = args[0] ?: "default"
+
+def jenkins = Jenkins.instance
+def viewName = '${environment}_csJobs'
+def existingView = jenkins.getView(viewName)
+
+if (existingView == null) {
+   def newView = new ListView(viewName)
+   jenkins.addView(newView)
+   println("View '$viewName' created.")
+   } else {
+   println("View '$viewName' already exists.")
+}
+
+matrixjob('${environment}_cs1stjob') {
+    description('This is an 1st cc Job DSL job')
+
+    configure { project ->
+        project / 'logRotator' {
+            numToKeep(4)
+                               }
+              }
+              
+    concurrentBuild(true)
+    
+    scm {
+    none()
+    }
+    
+    parameters {
+      stringParam('custName','NONE','desc')
+      }
+      
+    axes {
+         axis {
+             name('dyn')
+             valueString(dynamic)
+             }
+         userDefined('MY_AXIS_NAME') {
+             name('usr')
+             values('user')
+             }   
+         }
+
+
+def globalConfiguration = GlobalConfiguration.all().find { it.displayName == 'Mask Passwords and Regexes' }
+
+if (globalConfiguration) {
+           globalConfiguration.setMaskPasswords(true)
+           globalConfiguration.setMaskPasswordsConsoleLog(true)
+           globalConfiguration.save()
+           
+           System.setProperty("org.jenkinsci.plugins.workflow.steps.SecretEnvVarHelper.CONFIGURED", "true")
+           println("Enabled passwords")
+           } else {
+            println("Enabled passwordsnot found")
+                   }
+                   
+    def pName = "${custName}"
+
+    steps {
+        shell('echo ${environment} "Im Athna_cc_trigger" ${pName}')
+    }
+
+                   
+myView = hudson.model.Hudson.instance.getView('${environment}_csJobs')
+myView.doAddJobToView('${environment}_cs1stjob') 
+jenkins.save()
+}
+
