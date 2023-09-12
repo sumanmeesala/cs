f723df59c1d7b463e476c687a3a85189d32496a0
sumanmeesala <129158238+sumanmeesala@users.noreply.github.com>
2023-09-12 19:33:30 +0530

    Add files via upload

--- first.groovy
+++ first.groovy
@@ -32,11 +32,7 @@ matrixJob(jobName) {
 
     description('This is an 1st cc Job DSL job')
 
-    configure { project ->
-        project / 'logRotator' {
-            numToKeep(4)
-                               }
-              }
+
               
           println("job ' + $jobName + ' created-2.")
                   
